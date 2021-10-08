b4ef7374bbd941fa3af7a1e1394b68e2199d691b
Ana Carolina Arellano <pg20carolina@vfs.com>
2021-10-08 15:09:39 -0700

    Initial Commit

---
+++ SlangChallenge/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SlangChallenge
+//
+//  Created by Ana Carolina Arellano Alvarez on 08/10/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
